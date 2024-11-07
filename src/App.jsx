7f3cedcc54e3b437cf6104af8c67f3b933fa7ed5
Anwar <anwarmohammadi1390@gmail.com>
2024-11-07 13:44:48 +0430

    set Up the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ipsum,
+        asperiores quisquam laboriosam aliquid minus hic fuga beatae nam
+        repellendus, quo nobis, tempora repudiandae veritatis voluptatibus
+        consectetur a eveniet molestiae.
+      </p>
+    </div>
+  );
+};
+
+export default App;
